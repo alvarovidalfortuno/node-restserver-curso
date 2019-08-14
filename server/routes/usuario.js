78c18aa4af509b1c0d6208e24241a97c60bd3f3e
Alvaro Vidal <Alvaro Vidal@DesarrolloMB.local>
2019-08-14 18:14:16 -0400

    uso de XHR y google sign in

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -11,7 +11,12 @@ const Usuario = require('../models/usuario');
 
 const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion')
 
-
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 
 app.get('/usuario', (req, res) => {
 
