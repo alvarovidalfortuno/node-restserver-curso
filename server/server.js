b83ed92646be208bb7b3e523fb325b6d1e9b37ff
Alvaro Vidal <Alvaro Vidal@DesarrolloMB.local>
2019-08-14 17:42:43 -0400

    Allowing requests from any domain

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const path = require('path'); //Para usar ubicaciones locales y renderizar pági
 app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type', 'token');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
 
     next();
 })
