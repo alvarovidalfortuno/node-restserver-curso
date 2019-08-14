b6081ffd1809165a7608e14f7bd6e85595864e2a
Alvaro Vidal <Alvaro Vidal@DesarrolloMB.local>
2019-08-14 17:22:41 -0400

    Allowing requests from any domain

--- server/server.js
+++ server/server.js
@@ -10,6 +10,13 @@ const path = require('path'); //Para usar ubicaciones locales y renderizar pági
  * Inicializando el parsing para JSON
  */
 
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
